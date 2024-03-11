9d5028798bd8bf0d0545874a2dbecacd0eed325c
saba <gerund.grimmer.0j@icloud.com>
2024-03-12 02:24:11 +0900

    compile1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile RAILS_ENV=production
+bundle bundle exec rake assets:compile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
